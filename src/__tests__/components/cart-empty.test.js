af0899d944adbf63fb66dca857f2b918bc536955
Bukovski <kmanankov@bk.ru>
2021-11-22 11:20:00 +0300

    add: test component cart-empty

---
+++ src/__tests__/components/cart-empty.test.js
@@ -0,0 +1,15 @@
+import { screen } from "@testing-library/react";
+import { renderWithRouter } from "../../test-utils";
+import CartEmpty from "../../components/cart-empty.component";
+
+
+describe("CartEmpty Component", () => {
+	it("should render with Link", () => {
+		const { container } = renderWithRouter(<CartEmpty />);
+		
+		expect(screen.getByText(/Корзина пуста/)).toBeInTheDocument();
+		expect(screen.getByText('Вернуться назад').closest('a')).toHaveAttribute('href', '/')
+		
+		expect(container.firstChild).toMatchSnapshot()
+	})
+});
