3f4f2805c95375932dfe38a761439c08b8f4282c
Bukovski <kmanankov@bk.ru>
2021-11-26 10:10:00 +0300

    add: test pages not-found

---
+++ src/__tests__/pages/not-found.test.js
@@ -0,0 +1,13 @@
+import { render } from "@testing-library/react";
+import NotFound from "../../pages/not-found.page";
+
+
+describe("NotFound Page", () => {
+	it("should render", () => {
+		const { container } = render(<NotFound />);
+		
+		expect(container.firstChild).toHaveClass("main");
+		
+		expect(container.firstChild).toMatchSnapshot()
+	})
+});
